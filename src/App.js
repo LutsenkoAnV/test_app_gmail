38fa72ee963e39f3d2fa955b7718b40ca8749813
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-05-06 12:10:46 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import users from './api/users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spin</h1>
+    </div>
+  );
+}
+
+export default App;
